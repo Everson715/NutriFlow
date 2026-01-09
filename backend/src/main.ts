ad96149a51c8850eebaa2c789748f85be5066408
Everson <joseeverson757@gmail.com>
2026-01-08 23:34:16 -0300

    feat(auth): atualization CORS permission

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,6 +8,12 @@ const DEFAULT_PORT = 5000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  });
+
   app.use(cookieParser());
 
   app.useGlobalPipes(
